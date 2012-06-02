e0d6955d40dd15e4509713be59d3e3d59bcd9d8a
Arlo O'Keeffe <MailToArlo@googlemail.com>
2012-06-02 15:20:10 -0600

    No detail text in DeviceTable

--- IntAirActImageIOS/IADeviceTableViewController.m
+++ IntAirActImageIOS/IADeviceTableViewController.m
@@ -62,7 +62,6 @@ static const int ddLogLevel = LOG_LEVEL_WARN;
     // Configure the cell...
     IADevice * device = [self.devices objectAtIndex:indexPath.row];
     cell.textLabel.text = device.name;
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"http://%@:%i/", device.host, device.port];
     
     return cell;
 }
