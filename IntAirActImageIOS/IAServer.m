909d1cb8d3ce9d3c3ec6f640c54744900b73948a
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-10-30 14:26:24 -0600

    Properly answer requests

--- IntAirActImageIOS/IAServer.m
+++ IntAirActImageIOS/IAServer.m
@@ -57,12 +57,11 @@ static const int ddLogLevel = LOG_LEVEL_WARN;
     
     [self.intAirAct route:[IARoute routeWithAction:@"GET" resource:@"/images"] withHandler:^(IARequest *request, IAResponse *response) {
         DDLogVerbose(@"GET /images");
-        //[response respondWith:self.images withIntAirAct:self.intAirAct];
+        [response respondWith:self.images withIntAirAct:self.intAirAct];
     }];
     
-    [self.intAirAct route:[IARoute routeWithAction:@"GET" resource:@"/image/:id"] withHandler:^(IARequest *request, IAResponse *response) {
+    [self.intAirAct route:[IARoute routeWithAction:@"GET" resource:@"/image/:id.jpg"] withHandler:^(IARequest *request, IAResponse *response) {
         DDLogVerbose(@"GET /image/%@.jpg", request.parameters[@"id"]);
-        
         
         NSData * data = [self imageAsData: request.parameters[@"id"]];
         if (!data) {
