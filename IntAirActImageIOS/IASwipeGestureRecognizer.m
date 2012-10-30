f1d014a5c8a6774f7256cd2d8520a9e8ed57cbd7
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-10-30 13:54:11 -0600

    Made start and endpoint properties

--- IntAirActImageIOS/IASwipeGestureRecognizer.m
+++ IntAirActImageIOS/IASwipeGestureRecognizer.m
@@ -2,10 +2,10 @@
 
 #import <UIKit/UIGestureRecognizerSubclass.h>
 
-@interface IASwipeGestureRecognizer () {
-    CGPoint startPoint;
-    CGPoint endPoint;
-}
+@interface IASwipeGestureRecognizer ()
+
+@property (nonatomic) CGPoint startPoint;
+@property (nonatomic) CGPoint endPoint;
 
 @end
 
@@ -19,7 +19,7 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     UITouch * touch = [touches anyObject];
-    startPoint = [touch locationInView:touch.view];
+    self.startPoint = [touch locationInView:touch.view];
     [super touchesBegan:touches withEvent:event];
 }
 
@@ -28,7 +28,7 @@
     [super touchesMoved:touches withEvent:event];
     if (self.state ==  UIGestureRecognizerStateRecognized) {
         UITouch * touch = [touches anyObject];
-        endPoint = [touch locationInView:touch.view];
+        self.endPoint = [touch locationInView:touch.view];
         [self touchAngle];
     }
 }
@@ -45,10 +45,10 @@
 
 -(float)touchAngle
 {
-    // this is the swipe direction, it is in [-pi,pi]
-    float angle = atan2(-(endPoint.y - startPoint.y), endPoint.x - startPoint.x);
+    // this is the swipe direction, the result is between [-pi,pi]
+    float angle = atan2(-(self.endPoint.y - self.startPoint.y), self.endPoint.x - self.startPoint.x);
     
-    // change the angle to be from [0,2*pi]
+    // change the angle to be between [0,2*pi]
     if (angle < 0) {
         angle += M_PI * 2;
     }
